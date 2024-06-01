62bf8a754ee812235cc16bd82b413c7c98c05d2e
lzsfzlz <790381380@qq.com>
2024-06-01 22:42:59 +0800

    sfz_config.py

---
+++ sfz_config.py
@@ -0,0 +1,24 @@
+source_file = "sfz_demo.txt"
+final_file = "sfz_result.txt"
+favorite_list = [
+    "山东卫视",
+    "山东齐鲁",
+    "德州综合",
+    "CCTV-1",
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-13",
+    "CCTV-6",
+    "少儿频道",
+    "大湾区卫视",
+    "浙江卫视",
+    "湖南卫视",
+    "翡翠台",
+]
+favorite_page_num = 6
+default_page_num = 4
+urls_limit = 15
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
